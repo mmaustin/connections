02300ad647e4a7a23939d3b00c36500e21346fd9
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-11-03 21:15:55 -0500

    Stuff

--- connections-game/app/page.tsx
+++ connections-game/app/page.tsx
@@ -1,65 +1,44 @@
-import Image from "next/image";
+//import { Button } from "@/components/ui/button";
+'use client';
+
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 export default function Home() {
+
+  const handleColor = (e: React.MouseEvent<HTMLButtonElement>) => {
+    //e.currentTarget.classList.contains('border-blue-500')
+    if(e.currentTarget.classList.contains('bg-amber-200')){
+      e.currentTarget.classList.remove('bg-amber-200');
+      e.currentTarget.classList.add('bg-amber-500');
+    } else {
+      e.currentTarget.classList.remove('bg-amber-500');
+      e.currentTarget.classList.add('bg-amber-200');
+    }
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className=" h-screen w-full flex flex-col justify-center items-center" >
+      <p className="mb-5 text-4xl font-extrabold"> Black Connections</p>
+      <div className="w-[25%] h-[50%] border-2 border-green-700 grid grid-cols-4 gap-2 p-3">
+        <button onClick={handleColor} className="border border-red-700 bg-amber-200">TILE</button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700 bg-amber-200">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700 bg-amber-200">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700 bg-amber-200">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700 bg-amber-200">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700 bg-amber-200">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700 bg-amber-200">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700 bg-amber-200">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700 bg-amber-200">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700 bg-amber-200">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700">TILE</Button>
+        <Button onClick={handleColor} size="lg" variant="secondary" className="border border-red-700">TILE</Button>
+      </div>
+
     </div>
   );
 }
